63d4044217072a935deccf6c075d0f49c32b4a13
skin93 <arro.ciesielski@gmail.com>
2021-04-18 20:28:41 +0200

    Add client

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,7 @@ namespace API
             services.AddDbContext<DataContext>(options =>
                 options.UseSqlServer(_config.GetConnectionString("DatingAppDatabase")));
             services.AddControllers();
+            services.AddCors();
 
         }
 
@@ -46,6 +47,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
