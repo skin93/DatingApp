a0c57476e68dbf9e31834b0f68d28df9f9fa385a
skin93 <arro.ciesielski@gmail.com>
2021-04-19 14:02:29 +0200

    Add register form & home component

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -26,7 +26,18 @@ export class AccountService {
     )
   }
 
-  setCurrentUser(user: User){
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
     this.currentUserSource.next(user);
   }
 
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,22 +12,13 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
   ngOnInit() {
-   this.getUsers()
    this.setCurrentUser()
   }
 
   setCurrentUser(){
     const user: User = JSON.parse(localStorage.getItem('user'))
     this.accountService.setCurrentUser(user)
-  }
-
-  getUsers(){
-    this.http.get("https://localhost:5001/api/users").subscribe(res => {
-      this.users = res;
-    }, err => {
-      console.log(err)
-    })
   }
 }
